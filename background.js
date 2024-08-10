344dcfe31209990767c80ed289a00e9f026d485c
ainfante <adrianinfanteg@gmail.com>
2024-08-10 19:49:16 +0100

    modified url

--- background.js
+++ background.js
@@ -1,12 +1,19 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url && changeInfo.url.includes("https://www.youtube.com/watch?v")) {
-      let newUrl = changeInfo.url.replace("youtube.com", "yout-ube.com");
+  if (changeInfo.url && changeInfo.url.includes("https://www.youtube.com/watch?v")) {
+    // Extract the video ID from the URL
+    let videoId = new URL(changeInfo.url).searchParams.get("v");
+    
+    if (videoId) {
+      // Construct the new URL for youtube-nocookie.com
+      let newUrl = `https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1`;
+
+      // Update the tab with the new URL
       chrome.tabs.update(tabId, { url: newUrl }, () => {
         // Wait for a second before reloading the page
         setTimeout(() => {
           chrome.tabs.reload(tabId);
-        }, 1000); 
+        }, 1000);
       });
     }
-  });
-  
+  }
+});
